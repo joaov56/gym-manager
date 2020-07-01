36b2acd25c488ea699a2afbaf6237834efa8eeec
unknown <vitorjao56@gmail.com>
2020-07-01 13:58:36 -0300

    adiciona pagina edit e formatação de data

--- instructors.js
+++ instructors.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const data = require("./data.json");
+const { age, date } = require("./utils");
 
 //create
 exports.post = (req, res) => {
@@ -9,10 +10,22 @@ exports.post = (req, res) => {
     if (req.body[key] == "") return res.send("Please, fill all fields");
   }
 
-  req.body.birth = Date.parse(req.body.birth);
-  req.body.created_at = Date.now();
+  req.body.id = Number(data.instructors.length + 1);
 
-  data.instructors.push(req.body);
+  let { avatar_url, birth, name, gender, services } = req.body;
+  birth = Date.parse(birth);
+  const created_at = Date.now();
+  const id = Number(data.instructors.length + 1);
+
+  data.instructors.push({
+    id,
+    name,
+    avatar_url,
+    birth,
+    gender,
+    services,
+    created_at,
+  });
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
     if (err) {
       return res.send("Write file error");
@@ -21,6 +34,44 @@ exports.post = (req, res) => {
     return res.redirect("/instructors");
   });
 };
-//update
 
+//show
+exports.show = function (req, res) {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find(function (instructor) {
+    return instructor.id == id;
+  });
+
+  if (!foundInstructor) return res.send("Instructor not found");
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    services: foundInstructor.services.split(","),
+    created_at: new Intl.DateTimeFormat("pt-BR").format(
+      foundInstructor.created_at
+    ),
+  };
+
+  return res.render("./instructors/show", { instructor: instructor });
+};
+
+//update
+exports.edit = function (req, res) {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find(function (instructor) {
+    return instructor.id == id;
+  });
+
+  if (!foundInstructor) return res.send("Instructor not found");
+
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth),
+  };
+
+  return res.render("instructors/edit", { instructor: instructor });
+};
 //delete
